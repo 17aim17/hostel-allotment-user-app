90d072c4d0852b97383d103d59ed3991c6f7ae78
17aim17 <17aim17@gmail.com>
2020-07-16 17:24:30 +0530

    added _redirects file and changed firebase config for deploy

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,35 +1,26 @@
 import * as firebase from 'firebase'
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDx7w33T_qV83stiWrrC-NDmX3qGGyZ7EM",
-//     authDomain: "hostel-user.firebaseapp.com",
-//     databaseURL: "https://hostel-user.firebaseio.com",
-//     projectId: "hostel-user",
-//     storageBucket: "hostel-user.appspot.com",
-//     messagingSenderId: "954594383970",
-//     appId: "1:954594383970:web:8ed977058da5151b82993b",
-//     measurementId: "G-XX9PY4FDDV"
-// }
 const firebaseConfig = {
-    apiKey: "AIzaSyBpg2InuI8EiZhI6tgU0ZLR38WVIMtVwic",
-    authDomain: "micro-avenue-237301.firebaseapp.com",
-    databaseURL: "https://micro-avenue-237301.firebaseio.com",
-    projectId: "micro-avenue-237301",
-    storageBucket: "micro-avenue-237301.appspot.com",
-    messagingSenderId: "670302176078",
-    appId: "1:670302176078:web:dffbc02d069c7ac1a6141f",
-    measurementId: "G-WNSGJ143ES"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 firebase.initializeApp(firebaseConfig)
 
 const database = firebase.database();
 
 const auth = firebase.auth();
 
-// database.ref('admins').push({
-//     email: "17aim17@gmail.com",
-//     name: "Ashish Kumar"
-// })
+database.ref('admins').push({
+    email: "parveshwasuja@gmail.com",
+    name: "Parvesh Wasuja"
+})
 
 
 export { firebase, database, auth };
